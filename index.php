79c2a87c97c94f19ceb563cb6ae667d32c5a5a25
Habier <Habier@users.noreply.github.com>
2019-05-19 19:54:26 +0200

    Shared folder can be outside of public directory now

--- index.php
+++ index.php
@@ -1,41 +1,107 @@
 <?php
-$desired_ext=['rar','zip'];//Edit the extensions to your needs.
+/*
+ * Path to folder you want to list. Use getcwd() for current.
+ */
+$sharedFolder = getcwd();
 
-$allowedIps=[];//Public mode if left empty
-//Add your trusted IPs to the array to enable the "allowed only" mode
-//$allowedIps=['0.0.0.0', '192.168.1.35'];
+/*
+ * Edit the extensions to your needs, or leave empty to show everything
+ */
+$allowedFileExtensions = ['rar', 'zip'];
 
-if (!empty($allowedIps) )
-	if(!in_array($_SERVER['REMOTE_ADDR'], $allowedIps)){
-		header("HTTP/1.0 404 Not Found");
-		die();
-	}
-	
-function filesize_formatted($path)
-{	
-	/**
-		More about this function in
-		https://stackoverflow.com/questions/5501427/php-filesize-mb-kb-conversion 
-	**/
-    $size = sprintf('%u', filesize($path));
-    $units = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+/*
+ * Add your trusted IPs to the array to enable the "allowed only" mode, empty array means public mode
+ */
+$allowedIps = []; //For example $allowedIps=['0.0.0.0', '192.168.1.35'];
+
+
+/***********************************************************************************************************************
+ *  FUNCTIONS
+ **********************************************************************************************************************/
+
+/**
+ * @param $size
+ * @return string
+ */
+function formatFileSize($size)
+{
+    //Used this as base https://stackoverflow.com/questions/5501427/php-filesize-mb-kb-conversion
+    $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
     $power = $size > 0 ? floor(log($size, 1024)) : 0;
     return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
 }
 
-	
-$dir=getcwd();
-$files=scandir ($dir);
-
-foreach ($files as $file)
+/**
+ * @param $needle
+ * @param array $hayStack
+ * @return bool
+ */
+function findInArray($needle, Array $hayStack)
 {
-	$ext = pathinfo($file, PATHINFO_EXTENSION);
-	if (in_array ($ext, $desired_ext))
-	{
-		$size=filesize_formatted($dir.'/'.$file);
-		echo '['.$size.'] <a href="'.$file.'">'.$file.'</a><br>';
-	}
+    if (empty($hayStack))
+        return true;
+
+    return in_array($needle, $hayStack);
 }
 
+/**
+ * @param SplFileInfo $file
+ */
+function downloadFile($file)
+{
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename=' . $file->getFilename());
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . $file->getSize());
 
-?>
+    $fp = fopen($file->getRealPath(), 'rb');
+    ob_end_clean();
+    fpassthru($fp);
+
+    exit;
+}
+
+function notFoundError()
+{
+    header("HTTP/1.0 404 Not Found");
+    die();
+}
+
+/**********************************************************************************************************************
+ * MAIN
+ **********************************************************************************************************************/
+
+if (!findInArray($_SERVER['REMOTE_ADDR'], $allowedIps)) {
+    notFoundError();
+}
+
+$files = new FilesystemIterator($sharedFolder);
+
+if (isset($_GET['file'])) {
+    //A file has been requested
+
+    $requestedFileName = $_GET['file'];
+    foreach ($files as $file) {
+        /** @var SplFileInfo $file */
+        $fileName = $file->getFilename();
+        if (findInArray($file->getExtension(), $allowedFileExtensions)) {
+            if ($fileName == $requestedFileName)
+                downloadFile($file);
+        }
+    }
+    notFoundError();
+} else {
+    //No file requested, showing the list
+
+    foreach ($files as $file) {
+        /** @var SplFileInfo $file */
+        $fileName = $file->getFilename();
+        if (findInArray($file->getExtension(), $allowedFileExtensions)) {
+            $size = formatFileSize($file->getSize());
+            echo "[$size] <a href='?file=$fileName'>$fileName</a><br>";
+        }
+    }
+}
